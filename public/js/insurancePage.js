2f17d964667d4e1e04f88164fe89f13873570aa7
Ethan Zeronik <ejzeronik@gmail.com>
2020-10-02 12:22:07 -0400

    Finished basic insurance page, finsishing basic user page

--- public/js/insurancePage.js
+++ public/js/insurancePage.js
@@ -6,38 +6,55 @@ document.getElementsByTagName('head')[0].appendChild(script);
 
 var database = {};
 
-function requestDatabaseInformationReadOnly() {
-    $.get("/sql_database", function (data, status) {
+function requestDatabaseInformationReadOnly() 
+{
+    const queryString = window.location.search.replace("?userKey=","");
+
+    $.get("/insurance_page?userKey=" + encodeURIComponent(queryString), function (data, status) {
         database = data
         console.log(database);
+
+        //say hello
+        document.getElementById("topbar").innerHTML += '<div id="welcomeText"> Welcome: ' + database.User[0][2].value + " of " + database.InsuranceTable[0][1].value + "</div>";
+
         generateUserTable();
         generateInsurancePlanTable();
         generateInvoiceTable();
     });
+}
 
-    const queryString = window.location.search;
-    console.log(queryString);
+function payOutstandingInvoicesAuto()
+{
+    alert("Command has been sent to the database to be processed, database will be updated accordingly once the request copmpletes.");
+}
+
+function payOutstandingInvoice(index)
+{
+    if(confirm("You are confirming that the invoice is to be calculated and paid for the \nTreatment: " + database.InvoiceTable[index][0].value +  "\nHospital: " + database.InvoiceTable[index][1].value+  "\nPatient: " + database.InvoiceTable[index][2].value +  "\nProctor: " + database.InvoiceTable[index][3].value +  "\nAre you sure this is the correct invoice?"))
+    {
+        alert("Command has been sent to the database to be processed, database will be updated accordingly once the request copmpletes.");
+    }
 }
 
 function generateUserTable() {
     outputHtml = "<table> <tr>";
-    for (var i = 0; i < database.UserTable[0].length; i++) {
-        outputHtml += "<th>" + database.UserTable[0][i].metadata.colName + "</th>";
+    for (var i = 0; i < database.PatientTable[0].length; i++) {
+        outputHtml += "<th>" + database.PatientTable[0][i].metadata.colName + "</th>";
     }
     outputHtml += "</tr>";
     //iterate through the User table
-    for (var i = 0; i < database.UserTable.length; i++) {
+    for (var i = 0; i < database.PatientTable.length; i++) {
         // for each row (unique user)
         outputHtml += "<tr>";
-        for (var j = 0; j < database.UserTable[i].length; j++) {
+        for (var j = 0; j < database.PatientTable[i].length; j++) {
             // for each data point
-            outputHtml += "<td>" + database.UserTable[i][j].value + "</td>";
+            outputHtml += "<td>" + database.PatientTable[i][j].value + "</td>";
         }
         outputHtml += "</tr>";
     }
     outputHtml += "</table>";
     //add it to the div
-    document.getElementById("UserTable").innerHTML = outputHtml;
+    document.getElementById("UserTableInsurance").innerHTML += outputHtml;
 }
 
 function generateInsurancePlanTable() {
@@ -58,14 +75,24 @@ function generateInsurancePlanTable() {
     }
     outputHtml += "</table>";
     //add it to the div
-    document.getElementById("InsurancePlanTable").innerHTML = outputHtml;
+    document.getElementById("InsurancePlanTableInsurance").innerHTML += outputHtml;
 }
+
+
+
+function makeInvoiceButton(data)
+{
+    return  '<button type="button" onclick="payOutstandingInvoice('+ data +')" >Pay Invoice</button>';
+}
+
 
 function generateInvoiceTable() {
     outputHtml = "<table> <tr>";
     for (var i = 0; i < database.InvoiceTable[0].length; i++) {
         outputHtml += "<th>" + database.InvoiceTable[0][i].metadata.colName + "</th>";
     }
+    //will add teh button row
+    outputHtml += "<th> Pay Invoice </th>";
     outputHtml += "</tr>";
     //iterate through the User table
     for (var i = 0; i < database.InvoiceTable.length; i++) {
@@ -75,9 +102,10 @@ function generateInvoiceTable() {
             // for each data point
             outputHtml += "<td>" + database.InvoiceTable[i][j].value + "</td>";
         }
+        outputHtml += "<td>" + makeInvoiceButton(i) + "</td>";
         outputHtml += "</tr>";
     }
     outputHtml += "</table>";
     //add it to the div
-    document.getElementById("InvoiceTable").innerHTML = outputHtml;
+    document.getElementById("InvoiceTableInsurance").innerHTML += outputHtml;
 }
--- server.js
+++ server.js
@@ -40,68 +40,112 @@ app.listen(3000, function () {
     console.log('listening for website requests on port: 3000');
 });
 
-app.get('/?retry=false', function (req, res) {
-    res.sendFile(__dirname + "/public/index.html");
-});
-
-app.get('/database', function (req, res) {
-    res.sendFile(__dirname + "/public/database.html");
+app.get('/patient', function (req, res) {
+    res.sendFile(__dirname + "/public/patient.html");
 });
 
 app.get('/insurance', function (req, res) {
     res.sendFile(__dirname + "/public/insurance.html");
 });
 
-app.get('/patient', function (req, res) {
-    res.sendFile(__dirname + "/public/patient.html");
+app.get('/insurance_page', function (req, res) {
+    var userKey = req.query.userKey
+
+    endThis(userKey, function (val) {
+        let database = {};
+        database.User = val;
+        requestInsuranceFirm = new tedious.Request("SELECT * FROM InsuranceFirms WHERE InsuranceId  = '" + database.User[0][5].value + "';", function (err, rowCount, rows) {
+            database.InsuranceTable = rows;
+            //callback here
+            endthat(database, res)
+        });
+        requestPlans = new tedious.Request("SELECT * FROM InsurancePlans WHERE insuranceProviderId = '" + database.User[0][5].value + "' ORDER BY CoverageRate;", function (err, rowCount, rows) {
+            database.InsurancePlanTable = rows;
+            connection.execSql(requestInsuranceFirm);
+        });
+
+        connection.execSql(requestPlans);
+    });
+});
+
+function endMe(data) {
+    var stringPat = "('"
+    for (i = 0; i < data.PatientTable.length; i++) {
+        if (data.PatientTable.length > 0 && i != 0) {
+            stringPat += "','"
+        }
+        stringPat += data.PatientTable[i][0].value;
+    }
+    stringPat += "')";
+    return stringPat;
+}
+
+function endthat(data, r) {
+    var string = "('"
+
+    for (i = 0; i < data.InsurancePlanTable.length; i++) {
+        if (data.InsurancePlanTable.length > 0 && i != 0) {
+            string += "','"
+        }
+        string += data.InsurancePlanTable[i][0].value;
+    }
+    string += "')";
+
+    requestPatients = new tedious.Request("SELECT UserId, userCurrentInsurancePlan FROM Users WHERE userCurrentInsurancePlan IN " + string + ";", function (err, rowCount, rows) {
+        data.PatientTable = rows;
+        requestInvoices = new tedious.Request("SELECT * FROM Invoices WHERE patientId IN " + endMe(data) + ";", function (err, rowCount, rows) {
+            data.InvoiceTable = rows;
+            r.send(data)
+        });
+        connection.execSql(requestInvoices);
+    });
+    connection.execSql(requestPatients);
+}
+
+function endThis(userKey, callback) {
+    requestUser = new tedious.Request("SELECT * FROM Users WHERE UserId = CONVERT(uniqueidentifier,'" + userKey + "');", function (err, rowCount, rows) {
+        callback(rows)
+    });
+    connection.execSql(requestUser);
+}
+
+app.get('/database', function (req, res) {
+    res.sendFile(__dirname + "/public/database.html");
 });
 
 app.get('/sql_database', function (req, res) {
     var database = {};
     requestInsuranceFirms = new tedious.Request("SELECT * FROM InsuranceFirms ORDER BY FirmName;", function (err, rowCount, rows) {
-        if (err) {
-            console.log(err);
-        }
         database.InsuranceTable = rows;
         res.send(database);
     });
-    requestInvoices= new tedious.Request("SELECT * FROM Invoices ORDER BY IsPaidByPatient;", function (err, rowCount, rows) {
-        if (err) {
-            console.log(err);
-        }
+    requestInvoices = new tedious.Request("SELECT * FROM Invoices ORDER BY IsPaidByPatient;", function (err, rowCount, rows) {
         database.InvoiceTable = rows;
-        connection.execSql(requestInsuranceFirms); 
+        connection.execSql(requestInsuranceFirms);
     });
-    requestPlans= new tedious.Request("SELECT * FROM InsurancePlans ORDER BY CoverageRate;", function (err, rowCount, rows) {
-        if (err) {
-            console.log(err);
-        }
+    requestPlans = new tedious.Request("SELECT * FROM InsurancePlans ORDER BY CoverageRate;", function (err, rowCount, rows) {
         database.InsurancePlanTable = rows;
-        connection.execSql(requestInvoices); 
+        connection.execSql(requestInvoices);
     });
     requestTreatements = new tedious.Request("SELECT * FROM Treatments ORDER BY StandardPrice;", function (err, rowCount, rows) {
-        if (err) {
-            console.log(err);
-        }
         database.TreatmentTable = rows;
-        connection.execSql(requestPlans); 
+        connection.execSql(requestPlans);
     });
     requestHospitals = new tedious.Request("SELECT * FROM Hospitals ORDER BY HospitalName;", function (err, rowCount, rows) {
-        if (err) {
-            console.log(err);
-        }
         database.HospitalTable = rows;
-        connection.execSql(requestTreatements); 
+        connection.execSql(requestTreatements);
     });
     requestUsers = new tedious.Request("SELECT * FROM Users ORDER BY AccessLevel;", function (err, rowCount, rows) {
-        if (err) {
-            console.log(err);
-        }
         database.UserTable = rows;
-        connection.execSql(requestHospitals); 
+        connection.execSql(requestHospitals);
     });
-    
+
     connection.execSql(requestUsers);
+});
+
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/public/index.html");
 });
 
 app.post('/submit_login', function (req, res) {
@@ -111,7 +155,7 @@ app.post('/submit_login', function (req, res) {
     //log the data shown
     console.log(JSON.stringify(req.body) + " requested login");
     //check to see if the user is legit
-    request = new tedious.Request("SELECT AccessLevel FROM Users WHERE (UserName = '" + id + "') AND (Passkey = '" + passkey + "');", function (err, rowCount, rows) {
+    request = new tedious.Request("SELECT AccessLevel, UserId FROM Users WHERE (UserName = '" + id + "') AND (Passkey = '" + passkey + "');", function (err, rowCount, rows) {
         if (err) {
             console.log(err);
         }
@@ -122,16 +166,18 @@ app.post('/submit_login', function (req, res) {
                     res.redirect("/database");
                     break;
                 case 2:
-                    res.redirect("/insurance");
+                    var string = encodeURIComponent(passkey);
+                    res.redirect("/insurance?userKey=" + rows[0][1].value);
                     break;
                 case 3:
+                    res.redirect("/");
                     break;
                 case 4:
                     res.redirect("/patient");
                     break;
             }
         } else {
-            res.redirect("/?retry=true");
+            res.redirect("/");
         }
     });
     connection.execSql(request);
